30c279735ef3f2fdfbcdc2a276b4db569975973a
Maximovich <maksim.kob@icloud.com>
2024-09-08 19:19:52 +0300

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,11 @@
 module.exports = {
   content: ['./**/*.{html,js}'],
   theme: {
-    extend: {
-      colors: {
-        'active-color': '#1D293F',
-        'accent-color': '#FF7143',
-        'gray-color': '#696871',
-        'black-color': '#19191B',
-        'blue-color': '#1D293F',
-      },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
   },
   plugins: [],
