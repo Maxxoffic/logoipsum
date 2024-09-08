f724c80ecc8849a7e6c11dd1e47d0863a2e08d7a
Maximovich <maksim.kob@icloud.com>
2024-09-08 18:44:02 +0300

    tailwind deleta

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [".**/*.{html,js}"],
+  content: ['./**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'active-color': '#1D293F',
+        'accent-color': '#FF7143',
+        'gray-color': '#696871',
+        'black-color': '#19191B',
+        'blue-color': '#1D293F',
+      },
+    },
   },
   plugins: [],
 }
-
